03393ab509adb46e94504401c96d47e6cdd032c5
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-10-26 18:43:59 +0100

    Update mainClass.js

--- js/mainClass.js
+++ js/mainClass.js
@@ -83,3 +83,50 @@ formElement.addEventListener('submit', (event) => {
 window.onload = () => {
   bookObj.populateData();
 };
+
+// Neww
+
+const date = document.querySelector('.date');
+setInterval(() => {
+  date.innerHTML = new Date();
+}, 1000);
+
+const listSection = document.querySelector('#list-section');
+const formSection = document.querySelector('#form-section');
+const contactSection = document.querySelector('#contact-section');
+const listBtn = document.querySelector('#menu-list');
+const addFormBtn = document.querySelector('#menu-addnew');
+const contactBtn = document.querySelector('#menu-contact');
+
+
+
+const toggleWindow = () => {
+  listBtn.addEventListener('click', () => {
+    formSection.classList.add('hide');
+    contactSection.classList.add('hide');
+    listSection.classList.remove('hide');
+    listBtn.classList.add('active');
+    addFormBtn.classList.remove('active');
+    contactBtn.classList.remove('active');
+  });
+
+  addFormBtn.addEventListener('click', () => {
+    formSection.classList.remove('hide');
+    contactSection.classList.add('hide');
+    listSection.classList.add('hide');
+    listBtn.classList.remove('active');
+    addFormBtn.classList.add('active');
+    contactBtn.classList.remove('active');
+    
+  });
+
+  contactBtn.addEventListener('click', () => {
+    formSection.classList.add('hide');
+    contactSection.classList.remove('hide');
+    listSection.classList.add('hide');
+    listBtn.classList.remove('active');
+    addFormBtn.classList.remove('active');
+    contactBtn.classList.add('active');
+  });
+};
+toggleWindow();
