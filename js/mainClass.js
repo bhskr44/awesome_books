26fea33411fad50a8c3acb0f23ac2bbfac287d4d
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-10-26 20:38:13 +0530

    Update mainClass.js

--- js/mainClass.js
+++ js/mainClass.js
@@ -1,32 +1,4 @@
-class Books {
-  constructor(id, title, author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.books = localStorage.getItem('allBooks')
-      ? JSON.parse(localStorage.getItem('allBooks'))
-      : [];
-  }
 
-  removeBook(id) {
-    let allBooks = JSON.parse(localStorage.getItem('allBooks'));
-    let filtered = allBooks.filter((book) => book.id !== Number(id));
-    localStorage.setItem('allBooks', JSON.stringify(filtered));
-  }
-
-  saveBook(title, author) {
-    let allBooks = JSON.parse(localStorage.getItem('allBooks'));
-    if (allBooks === null) {
-      allBooks = [];
-    }
-    allBooks.push({
-      id: Date.now(),
-      title: title,
-      author: author,
-    });
-    localStorage.setItem('allBooks', JSON.stringify(allBooks));
-  }
-}
 
 let bookObj = new Books();
 const formElement = document.getElementById('formElement');
