11851143068929ecb7ae1525a028ef7a963c9943
BHASKAR RANJAN BORA <30873007+bhskr44@users.noreply.github.com>
2022-10-26 22:25:53 +0530

    Update mainClass.js

--- js/mainClass.js
+++ js/mainClass.js
@@ -26,6 +26,48 @@ class Books {
     });
     localStorage.setItem('allBooks', JSON.stringify(allBooks));
   }
+
+  populateData() {
+    const list = document.getElementById('list');
+    list.innerHTML = '';
+    let allBooks = JSON.parse(localStorage.getItem('allBooks'));
+    if (allBooks === null) {
+      allBooks = [];
+    }
+    if (allBooks.length > 0) {
+      list.classList.add('list');
+    } else {
+      list.classList.remove('list');
+    }
+
+    for (let i = 0; i < allBooks.length; i += 1) {
+      list.innerHTML +=
+        `<div class="book">
+        <span>"` +
+        allBooks[i].title +
+        `" by ` +
+        allBooks[i].author +
+        `</span>
+          <form action ="delete-book" class="remove-form" method="get">
+    
+        <input type="hidden" name="removeId" class="removeId" value="` +
+        allBooks[i].id +
+        `"></input>
+          <input type="submit" value="Remove"></input>
+        </form>
+          </div>`;
+    }
+
+    const removeBookEle = document.querySelectorAll('.remove-form');
+
+    removeBookEle.forEach((key) => {
+      key.addEventListener('submit', (event) => {
+        event.preventDefault();
+        this.removeBook(key.querySelector('.removeId').value);
+        this.populateData();
+      });
+    });
+  }
 }
 
 let bookObj = new Books();
@@ -35,9 +77,9 @@ formElement.addEventListener('submit', (event) => {
   const titleElement = document.getElementById('title');
   const authorElement = document.getElementById('author');
   bookObj.saveBook(titleElement.value, authorElement.value);
-  //   bookObj.populateData();
+  bookObj.populateData();
 });
 
 window.onload = () => {
-  //   bookObj.populateData();
+  bookObj.populateData();
 };
